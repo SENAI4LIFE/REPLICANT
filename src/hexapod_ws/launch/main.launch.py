a3907ed29ef63e1371c51a42625ec73686ab3dca
SENAI4LIFE <brunoalexandersenai@gmail.com>
2026-08-09 00:14:59 -0300

    World

--- src/hexapod_ws/launch/main.launch.py
+++ src/hexapod_ws/launch/main.launch.py
@@ -88,7 +88,7 @@ def generate_launch_description():
 
     world_arg = DeclareLaunchArgument(
         'world',
-        default_value='small_house',
+        default_value='living_room',
         description="World to load: 'living_room' (default), 'obstacle_arena', or 'small_house'",
     )
 
